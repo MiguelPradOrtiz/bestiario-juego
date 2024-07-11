56d91d6f38a5082a37560b58c4a5b906d714005c
Miguel <angelprado161@gmail.com>
2024-07-11 03:06:30 -0600

    3er Cambios en el Formulario y Card

--- bestiario/src/Card.jsx
+++ bestiario/src/Card.jsx
@@ -16,7 +16,9 @@ export default function Card({ data, setData }) {
     <div className="card-container">
       {data.map((monster) => (
         <div className="card" key={monster.id}>
-          <img className='img-monster' src={monster.url_imagen} alt={monster.nombre} />
+          <div className='img-container'>
+            <img className='img-monster' src={monster.url_imagen} alt={monster.nombre} />
+          </div>
           <h2>{monster.nombre}</h2>
           <div className="info-container">
             <p><strong>Nivel: </strong> {monster.level}</p>
@@ -27,7 +29,11 @@ export default function Card({ data, setData }) {
                 <li key={idx}>{strongItem}</li>
               ))}
             </ul>
-            <button onClick={() => handleDelete(monster.id)}>Eliminar</button>              
+            <button className='btn-delete' onClick={() => handleDelete(monster.id)}>
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 30 30">
+                <path d="M 13 3 A 1.0001 1.0001 0 0 0 11.986328 4 L 6 4 A 1.0001 1.0001 0 1 0 6 6 L 24 6 A 1.0001 1.0001 0 1 0 24 4 L 18.013672 4 A 1.0001 1.0001 0 0 0 17 3 L 13 3 z M 6 8 L 6 24 C 6 25.105 6.895 26 8 26 L 22 26 C 23.105 26 24 25.105 24 24 L 24 8 L 6 8 z"></path>
+              </svg>
+            </button>              
           </div>
         </div>
       ))}
--- bestiario/src/Formulario.jsx
+++ bestiario/src/Formulario.jsx
@@ -1,14 +1,24 @@
 import axios from 'axios';
 import React from 'react'
-import { useEffect, useState } from 'react';
+import { useState, useRef } from 'react';
 
 
 function Formulario({addMonster}) {
     const [nombre, setNombre] = useState('');
-    const [strong, setStrong] = useState('');
+    const [strong, setStrong] = useState([]);
     const [drops, setDrops] = useState('');
     const [level, setLevel] = useState('');
     const [imagen, setImagen] = useState(null);
+    const imagenRef = useRef(null);
+
+    const handleCheckboxChange = (e) => {
+      const value = e.target.value;
+      if (e.target.checked) {
+        setStrong([...strong, value]);
+      } else {
+        setStrong(strong.filter((item) => item !== value));
+      }
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -28,10 +38,12 @@ function Formulario({addMonster}) {
         .then(response => {
           addMonster(response.data);  // Agrega el nuevo monstruo al estado de App
           setNombre('');
-          setStrong('');
+          setStrong([]);
           setDrops('');
           setLevel('');
           setImagen(null);
+          imagenRef.current.value = '';
+          document.querySelectorAll('input[type=checkbox]').forEach(checkbox => checkbox.checked = false);
           
         })
         .catch(error => {
@@ -42,11 +54,20 @@ function Formulario({addMonster}) {
   return (
     <>
     <form className="form-container" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
-        <input type="text" placeholder="Resistencia" value={strong} onChange={(e) => setStrong(e.target.value)} required />
+        <input type="text" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />        
         <input type="text" placeholder="Drops" value={drops} onChange={(e) => setDrops(e.target.value)} required />
         <input type="number" min="0" placeholder="Nivel" value={level} onChange={(e) => setLevel(e.target.value)} required />
-        <input type="file" onChange={(e) => setImagen(e.target.files[0])} required />
+        <div className="checkbox-container">
+          <span>Resistencias: </span>
+          {["Fuego", "Agua", "Rayo", "Hielo", "Viento", "Oscuridad", "Sagrado"].map((elemento, index) => (
+            <label key={index} className={`checkbox-label ${elemento.toLowerCase()}`}>
+              <input type="checkbox" value={elemento} onChange={handleCheckboxChange} />
+              <span>{elemento}</span>
+            </label>
+          ))}
+        </div>
+        <input type="file" ref={imagenRef} onChange={(e) => setImagen(e.target.files[0])} required />
+        
         <button type="submit">Agregar Monstruo</button>
     </form>
     </>
